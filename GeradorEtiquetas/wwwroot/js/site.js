ef9a6923dddaf31d83f4be2b40d7016dbd744efb
Adam <adam.machado12@gmail.com>
2023-06-19 01:50:26 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorEtiquetas/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿function buscarCEP(tipo) {
+    var cep = $("#" + tipo + "_Cep").val();
+
+    $.ajax({
+        url: "/Etiqueta/BuscarCEP",
+        type: "GET",
+        data: { cep: cep },
+        success: function (endereco) {
+            if (endereco != null) {
+                $("#" + tipo + "_Logradouro").val(endereco.logradouro);
+                $("#" + tipo + "_Bairro").val(endereco.bairro);
+                $("#" + tipo + "_Localidade").val(endereco.localidade);
+                $("#" + tipo + "_Uf").val(endereco.uf);
+            } else {
+                swal("Alerta!", "CEP não encontrado.", "warning");
+            }
+        },
+        error: function () {
+            swal("Erro!", "Ocorreu um erro ao buscar o CEP.", "error");
+        }
+    });
+}
+
+// Funcionalidade para imprimir ao pressionar o botão
+document.getElementById('btnImprimir').addEventListener('click', function () {
+    this.style.display = 'none';
+    window.print();
+});
